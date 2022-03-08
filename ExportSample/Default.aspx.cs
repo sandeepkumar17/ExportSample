22521e75d8f0f720172c2a45085d379afef9055e
Sandeep Kumar <sandeep.kumar.17@gmail.com>
2022-03-08 20:17:47 +0530

    Added Initial code

---
+++ ExportSample/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using ExportSample.Service;
+using System;
+using System.Web.UI;
+
+namespace ExportSample
+{
+    public partial class _Default : Page
+    {
+        #region ===[ Page Events ]=================================================================
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region ===[ Control Events ]==============================================================
+
+        protected void BtnExportReportClick(object sender, EventArgs e)
+        {
+            //Export file.
+            switch (ddlReportType.SelectedValue)
+            {
+                case "1":
+                    ExportService.ExportExcelFile();
+                    break;
+                case "2":
+                    ExportService.ExportCsvFile();
+                    break;
+                case "3":
+                    ExportService.ExportTextFile();
+                    break;
+                case "4":
+                    ExportService.ExportPdfFile();
+                    break;
+                case "5":
+                    ExportService.ExportHtmlFile();
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ ExportSample/Service/DataService.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace ExportSample.Service
+{
+    public static class DataService
+    {
+        #region ===[ Public Methods ]=============================================================
+
+        /// <summary>
+        /// Get Record List.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<DataProxy> GetData()
+        {
+            return new List<DataProxy>
+                   {
+                       new DataProxy
+                           {
+                               FirstName = "Sandeep",
+                               LastName = "Kumar",
+                               Email = "test1@email.com",
+                               City = "Gurgaon"
+                           },
+                       new DataProxy
+                           {
+                               FirstName = "FName1",
+                               LastName = "Lname1",
+                               Email = "test2@email.com",
+                               City = "New Delhi"
+                           },
+                       new DataProxy
+                           {
+                               FirstName = "FName4",
+                               LastName = "Lname2",
+                               Email = "test3@email.com",
+                               City = "Mumbai"
+                           },
+                       new DataProxy
+                           {
+                               FirstName = "FName3",
+                               LastName = "Lname3",
+                               Email = "test4@email.com",
+                               City = "Mumbai"
+                           },
+                       new DataProxy
+                           {
+                               FirstName = "FName4",
+                               LastName = "Lnam4",
+                               Email = "test5@email.com",
+                               City = "Gurgaon"
+                           }
+                   };
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Proxy class
+    /// </summary>
+    public sealed class DataProxy
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+    }
+}
